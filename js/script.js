4af4df0b1e08360e6f1aaf71686c11b26cab0b1a
codeComrade <110293984+codeComrade777@users.noreply.github.com>
2022-09-13 14:35:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,22 +26,29 @@ window.onscroll=()=>{
         body.classList.remove('active');
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let toggleBtn=document.querySelector('#toggle-btn');
+let darkMode=localStorage.getItem('dark-mode');
+const enableDarkMode=()=>{
+    toggleBtn.classList.replace('fa-sun','fa-moon');
+    body.classList.add('dark');
+    localStorage.getItem('dark-mode','enabled');
+}
+const disableDarkMode=()=>{
+    toggleBtn.classList.replace('fa-moon','fa-sun');
+    body.classList.remove('dark');
+    localStorage.getItem('dark-mode','disabled');
+}
+if(darkMode=='enabled'){
+    enableDarkMode();
+}
+toggleBtn.onclick=(e)=>{
+    let darkMode=localStorage.getItem('dark-mode');
+    if(darkMode=='disabled'){
+        enableDarkMode();
+    }
+    else
+    {
+        disableDarkMode();
+    }
+}
 
